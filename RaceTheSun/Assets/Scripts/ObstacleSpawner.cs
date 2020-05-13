218ec0657ca42f4524feffea3a607cdfe6362f62
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-05-13 13:01:54 +0200

    Adding The Main Game
    Adding the Unity Project to the Repository

---
+++ RaceTheSun/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public float maxTime = 1;
+    public float timer = 0;
+    public GameObject obstacles;
+    public float displacement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObstacles = Instantiate(obstacles);
+        newObstacles.transform.position = transform.position + new Vector3(Random.Range(-displacement, displacement),6,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newObstacles = Instantiate(obstacles);
+            newObstacles.transform.position = transform.position + new Vector3(Random.Range(-displacement, displacement), 6, 0);
+            Destroy(newObstacles, 8);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
---
+++ RaceTheSun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    public Rigidbody2D playerRB;
+
+    public GameObject gameOverCanvas;
+    public GameObject tutorialCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB.gravityScale = 0;
+        Time.timeScale = 1;
+        gameOverCanvas.SetActive(false);
+
+        Invoke("TutorialDisappear", 5);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            playerRB.velocity = new Vector2(-movementSpeed, playerRB.velocity.y);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            playerRB.velocity = new Vector2(movementSpeed, playerRB.velocity.y);
+        }
+        else if(!Input.GetMouseButton(1) && !Input.GetMouseButton(0))
+        {
+            playerRB.velocity = new Vector2(0, playerRB.velocity.y);
+
+        }
+
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Obstacles")
+        {
+            gameOverCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    private void TutorialDisappear()
+    {
+        tutorialCanvas.SetActive(false);
+    }
+}
